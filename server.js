7a43636741b089baafcc9e38eb6a20105e62f57c
James White <james@llamasantos.com>
2015-03-13 14:41:18 -0600

    basic stuffs setup

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 import Hapi from 'hapi';
+import stream from 'stream';
 
 let server = new Hapi.Server();
 server.connection({ port: process.env.PORT || 8081 });
+
+server.route({
+  method: 'GET',
+  path: '/data-stream',
+  handler (req, reply) {
+    let channel = new stream.PassThrough
+    let raw = req.raw.req;
+    // -- Keep the connection open and don't timeout.
+    raw.socket.setTimeout(Infinity);
+
+    setTimeout(() => {
+      channel.write('Hello\n');
+      console.info('writing data...');
+    }, 100)
+
+    reply(channel)
+      .header('Content-Type', 'text/event-stream')
+      .header('Cache-Control', 'no-cache')
+      .header('Connection', 'keep-alive');
+
+    raw.on('close', () => {
+      console.info('Client connection closed');
+    });
+  }
+});
 
 server.start(() => {
   console.info(`Server running at: ${server.info.uri}`);
