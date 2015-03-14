72e0eed3e41baaeee42e5accdc4e1027df40a28f
Dane Thurber <dane.thurber@gmail.com>
2015-03-13 19:51:28 -0600

    stash

--- client/src/js/common/api.js
+++ client/src/js/common/api.js
@@ -1,21 +1,19 @@
 import { EventEmitter } from 'events';
 import assign from 'react/lib/Object.assign';
 
-var dummyData = [
-  { id: 1, title: 'Ipsum Cursus Venenatis Etiam' },
-  { id: 2, title: 'Nullam Nibh Pharetra Vulputate' }
-];
-
 var API = assign({}, EventEmitter.prototype, {
 
   start () {
-    setTimeout(() => {
-      this.emit('success', dummyData);
-    }, 1000);
+    this.source = new EventSource('/data-stream');
+
+    this.source.addEventListener('message', function(e) {
+      console.log('received message');
+      console.log(arguments);
+    }, false);
   },
 
   stop () {
-    console.log('stop api');
+    this.source.close();
   }
 
 });
